21bfc23d17c03f5811fc3c6f295fa3d631296665
felipevolpone <felipevolpone@gmail.com>
2016-01-09 18:34:12 -0200

    tests json util

--- tests/test_json_util.py
+++ tests/test_json_util.py
@@ -1,6 +1,13 @@
 import unittest
-from util import json_util
-from tests.mock import Person
+from onhands import json_util
+from onhands.endpoint import endpoint
+from alabama.models import StringProperty, IntegerProperty, BaseModel
+
+
+@endpoint('/user')
+class UserModel(BaseModel):
+    name = StringProperty()
+    age = IntegerProperty()
 
 
 class TestJson(unittest.TestCase):
@@ -8,7 +15,7 @@ class TestJson(unittest.TestCase):
     def test_from_list(self):
         persons = []
         for i in range(0, 2):
-            persons.append(Person(name='felipe', age=i))
+            persons.append(UserModel(name='felipe', age=i))
 
         persons_json = json_util.from_list(persons)
         self.assertEqual([{'name': 'felipe', 'age': 0, 'id': None},
