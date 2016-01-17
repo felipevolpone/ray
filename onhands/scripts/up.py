2f2e6bd62d356ff81e9c3a254349d1765e0a2cd3
felipevolpone <felipevolpone@gmail.com>
2016-01-17 18:13:41 -0200

    Changing the way that wsgi reads config file

--- onhands/scripts/up.py
+++ onhands/scripts/up.py
@@ -1,10 +1,13 @@
 import click, os
 
 @click.command()
-@click.option('--ini', default="onhands/wsgi/dev.ini", help='uwsgi ini file')
+@click.option('--ini', default=None, help='uwsgi ini file')
 @click.option('--pid', default='dev_onhands.pid', help='pid file')
-              
-def cli(ini, pid):
-    to_run = 'uwsgi  --ini ' + ini + ' --pidfile ' + pid
-    print to_run
+@click.option('--wsgifile', help='the wsgi.py file that starts application')
+
+def cli(ini, pid, wsgifile):
+    to_run = 'uwsgi --http=0.0.0.0:8080 --wsgi-file='+wsgifile+' --threads=2 --py-autoreload=1'
+    if ini:
+        to_run = 'uwsgi  --ini ' + ini + ' --pidfile ' + pid
+
     os.system(to_run)
--- onhands/wsgi/wsgi.py
+++ onhands/wsgi/wsgi.py
@@ -1,4 +1,4 @@
 import webapp2
 from onhands.api import ApiHandler
 
-app = webapp2.WSGIApplication([('/api/.*', ApiHandler)])
+application = webapp2.WSGIApplication([('/api/.*', ApiHandler)])
