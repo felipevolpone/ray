19f4c3d5baab7dce823bb3103743f84b0d0a588f
felipevolpone <felipevolpone@gmail.com>
2016-01-17 17:59:08 -0200

    fixing up server command

--- onhands/commandline.py
+++ onhands/commandline.py
@@ -1,14 +1,13 @@
-import click, os
+import click, os, sys
 
 cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
                                           'scripts'))
+
 
 class OnHandsCLI(click.MultiCommand):
 
     def list_commands(self, ctx):
         rv = []
-        print cmd_folder
-        print os.listdir(cmd_folder)
         for filename in os.listdir(cmd_folder):
             if filename.endswith('.py'):
                 rv.append(filename[:-3])
@@ -19,8 +18,8 @@ class OnHandsCLI(click.MultiCommand):
         try:
             if sys.version_info[0] == 2:
                 name = name.encode('ascii', 'replace')
-            mod = __import__('onhands.scripts.cmd_' + name,
-                             None, None, ['cli'])
+            mod = __import__('onhands.scripts.' + name,
+                             None, None, ['onhands.commandline'])
         except ImportError:
             return
         return mod.cli
@@ -29,3 +28,4 @@ class OnHandsCLI(click.MultiCommand):
 @click.command(cls=OnHandsCLI)
 def interface():
     pass
+
--- onhands/scripts/up.py
+++ onhands/scripts/up.py
@@ -4,7 +4,7 @@ import click, os
 @click.option('--ini', default="onhands/wsgi/dev.ini", help='uwsgi ini file')
 @click.option('--pid', default='dev_onhands.pid', help='pid file')
               
-def up(ini, pid):
+def cli(ini, pid):
     to_run = 'uwsgi  --ini ' + ini + ' --pidfile ' + pid
     print to_run
     os.system(to_run)
