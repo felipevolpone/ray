3178379b36c80e1a334686ceecc25deb04e9a0d5
Felipe Barreto Volpone <felipevolpone@gmail.com>
2016-01-28 08:17:49 -0200

    Adding description to onhands-core at setup.py. Configuring the Dockerfile of onhands-alabama

--- onhands-core/setup.py
+++ onhands-core/setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup
 
+
+def description():
+    return """
+    Python on Hands is a framework that helps you to deliver
+    well designed software without been stucked in your framework.
+    On Hands it's a ready to production framework that contains
+    a uWSGI server that can be used in production as well.
+    """
+
 setup(
     name="OnHands",
     version="0.0.1",
     author="Felipe Volpone",
     author_email="felipevolpone@gmail.com",
-    description=(""),
-    license = "MIT",
-    keywords = "python framework onhands api rest",
-    url = "http://github.com/felipevolpone/onhands",
+    description=description(),
+    license="MIT",
+    keywords="python framework onhands api rest",
+    url="http://github.com/felipevolpone/onhands",
     packages=['onhands'],
     install_requires=['webapp2', 'webob', 'click'],
     long_description="Check on github: http://github.com/felipevolpone/onhands",
