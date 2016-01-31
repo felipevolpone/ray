17d65f269f0151e6220616f014bf68e2e107cc01
felipevolpone <felipevolpone@gmail.com>
2016-01-31 19:18:52 -0200

    onhands-alabama version

--- onhandsalabama/setup.py
+++ onhandsalabama/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="onhands-alabama",
-    version="0.0.1",
+    version="0.0.2",
     author="Felipe Volpone",
     author_email="felipevolpone@gmail.com",
     description=(""),
