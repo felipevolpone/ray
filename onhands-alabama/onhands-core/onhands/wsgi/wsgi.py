8da14d2fd9c691da023c3faf9a9b7f05b5d3f157
felipevolpone <felipevolpone@gmail.com>
2016-02-09 19:41:32 -0200

    deleting onhands-core for the wrong place

--- onhands-alabama/onhands-core/onhands/wsgi/wsgi.py
+++
@@ -1,4 +0,0 @@
-import webapp2
-from onhands.api import ApiHandler
-
-application = webapp2.WSGIApplication([('/api/.*', ApiHandler)])
--- onhands-alabama/onhands-core/tests/uwsgi.py
+++
@@ -1,4 +0,0 @@
-import webapp2
-
-def create_application(app, paths):
-    application = webapp2.WSGIApplication(paths)
