2c93c291fab756db7fbebfe9df03fec24526fb9e
felipevolpone <felipevolpone@gmail.com>
2016-02-09 19:57:23 -0200

    removing absolute paths when runing tests.

--- onhands-alabama/tests/run_tests.sh
+++ onhands-alabama/tests/run_tests.sh
@@ -1,5 +1,5 @@
 docker rm -f "onhands-alabama"
-cp -r ~/projetos/onhands/onhands-core tests/
+cp -r ../onhands-core tests/
 cp -r onhandsalabama/ tests/
 docker run -i -t -v "$(pwd):/home/onhands-test/"  --name "onhands-alabama" "onhands-alabama" sh -c "cd tests; ./test.sh"
 rm -rf tests/onhands-core
