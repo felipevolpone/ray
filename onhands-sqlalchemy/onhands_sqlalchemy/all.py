0614b315f44c7829cfbf1ad0954401f07d48040f
Felipe Barreto Volpone <felipevolpone@gmail.com>
2016-02-11 19:26:37 -0200

    sqlalchemy integration

---
+++ onhands-sqlalchemy/onhands_sqlalchemy/all.py
@@ -0,0 +1,11 @@
+
+from onhands.model import BaseModel
+
+
+class AlchemyModel(object):
+
+    def describe(self):
+        pass
+
+    def columns(self):
+        pass
---
+++ onhands-sqlalchemy/tests/test_integrated.py
@@ -0,0 +1,8 @@
+
+import unittest
+
+
+class TestIntegrated(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(1, 1)
