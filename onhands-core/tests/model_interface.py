cc8accd243970b3f053c456c7f9467c138d2dedc
felipevolpone <felipevolpone@gmail.com>
2016-02-10 22:02:26 -0200

    model interface to tests

---
+++ onhands-core/tests/model_interface.py
@@ -0,0 +1,29 @@
+from onhands.model import Model
+import uuid
+
+
+def gen_uuid():
+    return str(uuid.uuid4())
+
+
+class ModelInterface(Model):
+
+    def __init__(self, *a, **k):
+        super(ModelInterface, self).__init__(*a, **k)
+        self.id = None
+
+    def columns(self):
+        return self.describe().keys()
+
+    def put(self, *args, **kwargs):
+        self.uuid = gen_uuid()
+        return self
+
+    def get(self, *args, **kwargs):
+        return self
+
+    def find(self, *args, **kwargs):
+        return [self]
+
+    def delete(self, *args, **kwargs):
+        return self
