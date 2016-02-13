6a407438641e6d92a734d05ef9e5c89395a40a9a
felipevolpone <felipevolpone@gmail.com>
2016-02-13 16:22:39 -0200

    onhands-sqlalchemy delete

--- onhands-sqlalchemy/tests/test_integrated.py
+++ onhands-sqlalchemy/tests/test_integrated.py
@@ -10,13 +10,15 @@ class TestIntegrated(unittest.TestCase):
         columns = User.columns()
         self.assertEqual(['age', 'id', 'name'], columns)
 
+    def _create(sefl, name=name, age=age):
+        data = {'age': age, 'name': name}
+        return requests.post('http://localhost:8080/api/user', data=data)
+
     def test_api(self):
-        data = json.dumps({'age': 22, 'name': 'felipe'})
-        resp = requests.post('http://localhost:8080/api/user', data=data)
+        resp = self._create(name='felipe', age=22)
         result = json.loads(resp.content)['result']
         self.assertEqual(result['name'], 'felipe')
         self.assertEqual(result['age'], 22)
-
         self.assertIsNotNone(result['id'])
         id_created = str(result['id'])
 
@@ -25,6 +27,12 @@ class TestIntegrated(unittest.TestCase):
         self.assertEqual(result[0]['name'], 'felipe')
         self.assertEqual(result[0]['age'], 22)
         self.assertIsNotNone(result[0]['id'])
+
+        self._create(name='john', age=26)
+        resp = requests.get('http://localhost:8080/api/user' + id_created)
+        result = json.loads(resp.content)['result']
+        self.assertEqual('felipe', result['name'])
+        self.assertEqual(22, result['age'])
 
         resp = requests.delete('http://localhost:8080/api/user/' + id_created)
         print resp.json()
