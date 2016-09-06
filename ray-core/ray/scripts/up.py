2bbad8c0e0988daabb818c961b5f96324751e419
Felipe Volpone <felipevolpone@gmail.com>
2016-09-06 20:31:22 -0300

    Fixing #19 ray up command

--- ray-core/ray/scripts/up.py
+++ ray-core/ray/scripts/up.py
@@ -2,22 +2,16 @@ import click, os
 
 
 @click.command()
-@click.option('--ini', default=None, help='uwsgi ini file')
-@click.option('--pid', default='dev_ray.pid', help='pid file')
 @click.option('--env', help='the virtualenv dir')
 @click.option('--wsgifile', help='the wsgi.py file that starts application')
-def cli(ini, pid, env, wsgifile=None):
+def cli(env, wsgifile):
 
     if not wsgifile:
-        raise Exception('you have to provide the path o wsgi.py file')
+        raise Exception('you have to provide the path of the wsgi file that has the application')
 
-    to_run = 'uwsgi --http=0.0.0.0:8080 --wsgi-file=' + wsgifile + ' --threads=2 --py-autoreload=1 --pidfile=' + pid
+    to_run = 'uwsgi --http=0.0.0.0:8080 --wsgi-file=' + wsgifile
 
     if env:
-        to_run += ' --virtualenv=' + env
+        to_run += ' -H=' + env
 
-    if ini:
-        to_run = 'uwsgi  --ini ' + ini + ' --pidfile ' + pid
-
-    print(to_run)
     os.system(to_run)
