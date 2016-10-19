3edd3350005869f8730b8a8a07bdfb922ec34de5
Felipe Volpone <felipevolpone@gmail.com>
2016-10-19 07:59:39 -0200

    Landscape config

--- examples/gae-example/src/models.py
+++ examples/gae-example/src/models.py
@@ -1,7 +1,6 @@
 
 from ray.hooks import Hook
 from ray.endpoint import endpoint
-from ray.wsgi.wsgi import application
 
 from ray_appengine.all import GAEModel
 from google.appengine.ext import ndb
