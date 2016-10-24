615ade50e8997c4d06dcba9aceb6f018b5d967fa
Felipe Barreto Volpone (DEXTRA) <felipe.volpone@dextra-sw.com>
2016-10-24 19:39:00 -0200

    Moving sqlalchemy to python3

--- ray-sqlalchemy/tests/test_integrated.py
+++ ray-sqlalchemy/tests/test_integrated.py
@@ -1,29 +1,8 @@
 
-import unittest
-import json
+import unittest, json
+from webtest import TestApp
 from .app import User
-from webapp2 import Request
-from ray.http import Response
 from .app import application
-
-
-class MockResponse(Response):
-
-    def __init__(self, instance):
-        Response.__init__(self, instance.json)
-
-    def to_json(self):
-        result = {}
-        if not self._json:
-            return {}
-
-        for key, value in self._json.items():
-            key = key.encode('utf-8')
-            if type(value) is unicode:
-                value = value.encode('utf-8')
-
-            result[key] = value
-        return result
 
 
 def build_url(id_=None, params=None):
@@ -35,18 +14,20 @@ def build_url(id_=None, params=None):
 
 class TestIntegrated(unittest.TestCase):
 
+    def setUp(self):
+        self.app = TestApp(application)
+
     def test_columns(self):
         self.assertEqual(['age', 'id', 'name'], User.columns())
 
     def _create(self, name=None, age=None):
-        request = Request.blank(build_url(), method='POST')
-        request.json = {'age': age, 'name': name}
-        return MockResponse(request.get_response(application))
+        return self.app.post_json(build_url(), {'age': age, 'name': name})
 
     def test_api(self):
         # test post create
         resp = self._create(name='felipe', age=22)
-        result = resp.to_json()['result']
+        result = resp.json['result']
+
         self.assertEqual(result['name'], 'felipe')
         self.assertEqual(result['age'], 22)
         self.assertIsNotNone(result['id'])
@@ -55,79 +36,73 @@ class TestIntegrated(unittest.TestCase):
         self._create(name='john', age=26)
 
         # test get all
-        req = Request.blank(build_url(), method='GET')
-        resp = MockResponse(req.get_response(application))
-        result = resp.to_json()['result']
+        resp = self.app.get(build_url())
+        result = resp.json['result']
         self.assertEqual(result[0]['name'], 'felipe')
         self.assertEqual(result[0]['age'], 22)
         self.assertIsNotNone(result[0]['id'])
         self.assertEqual(result[1]['name'], 'john')
 
         # test get by id
-        req = Request.blank(build_url(id_created), method='GET')
-        resp = MockResponse(req.get_response(application))
-        result = resp.to_json()['result']
+        resp = self.app.get(build_url(id_created))
+        result = resp.json['result']
         self.assertEqual('felipe', result['name'])
         self.assertEqual(22, result['age'])
 
         # test update
-        req = Request.blank(build_url(id_created), method='PUT')
-        req.json = {'name': 'felipe volpone'}
-        resp = MockResponse(req.get_response(application))
-        self.assertEqual(200, resp.status_code)
+        resp = self.app.put_json(build_url(id_created), {'name': 'felipe volpone'})
+        self.assertEqual(200, resp.status_int)
 
-        req = Request.blank(build_url(id_created), method='GET')
-        resp = MockResponse(req.get_response(application))
-        result = resp.to_json()['result']
+        resp = self.app.get(build_url(id_created))
+        result = resp.json['result']
         self.assertEqual('felipe volpone', result['name'])
         self.assertEqual(22, result['age'])
         self.assertEqual(int(id_created), result['id'])
 
         # test delete
-        req = Request.blank(build_url(id_created), method='DELETE')
-        resp = req.get_response(application)
-        self.assertEqual(200, resp.status_code)
+        resp = self.app.delete(build_url(id_created))
+        self.assertEqual(200, resp.status_int)
 
         # test get
-        req = Request.blank(build_url(id_created), method='GET')
-        resp = req.get_response(application)
-        self.assertEqual(404, resp.status_code)
+        resp = self.app.get(build_url(id_created), expect_errors=True)
+        self.assertEqual(404, resp.status_int)
 
 
-@unittest.skip('skip')
 class TestWhereAtAPI(unittest.TestCase):
 
+    def setUp(self):
+        self.app = TestApp(application)
+
     def _create(self, name=None, age=None):
-        data = {'age': age, 'name': name}
-        return requests.post(build_url(), data=data)
+        return self.app.post_json(build_url(), {'age': age, 'name': name})
 
     def test_query_params(self):
         for name, age in [('felipe', 35), ('joao', 23), ('roberto', 55), ('roberto', 53)]:
             self._create(name=name, age=age)
 
-        resp = requests.get(build_url(params="?name=felipe"))
-        self.assertEqual(200, resp.status_code)
-        result = resp.to_json()['result']
+        resp = self.app.get(build_url(params="?name=felipe"))
+        self.assertEqual(200, resp.status_int)
+        result = resp.json['result']
         self.assertEqual(1, len(result))
         self.assertEqual(result[0]['name'], 'felipe')
         self.assertEqual(result[0]['age'], 35)
 
-        resp = requests.get(build_url(params="?age=23"))
-        self.assertEqual(200, resp.status_code)
-        result = json.loads(resp.content)['result']
+        resp = self.app.get(build_url(params="?age=23"))
+        self.assertEqual(200, resp.status_int)
+        result = resp.json['result']
         self.assertEqual(1, len(result))
         self.assertEqual(result[0]['name'], 'joao')
 
-        resp = requests.get(build_url(params="?name=roberto"))
-        self.assertEqual(200, resp.status_code)
-        result = json.loads(resp.content)['result']
+        resp = self.app.get(build_url(params="?name=roberto"))
+        self.assertEqual(200, resp.status_int)
+        result = resp.json['result']
         self.assertEqual(2, len(result))
         self.assertEqual(result[0]['age'], 55)
         self.assertEqual(result[1]['age'], 53)
 
-        resp = requests.get(build_url(params="?name=roberto&age=53"))
-        self.assertEqual(200, resp.status_code)
-        result = json.loads(resp.content)['result']
+        resp = self.app.get(build_url(params="?name=roberto&age=53"))
+        self.assertEqual(200, resp.status_int)
+        result = resp.json['result']
         self.assertEqual(1, len(result))
         self.assertEqual(result[0]['name'], 'roberto')
         self.assertEqual(result[0]['age'], 53)
