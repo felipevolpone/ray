e318a891846d7d6622e9128e8e8f0d1941a8222d
Felipe Barreto Volpone (DEXTRA) <felipe.volpone@dextra-sw.com>
2016-10-24 18:35:25 -0200

    Fixing how to create the env

--- venv/create_env.sh
+++ venv/create_env.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
-virtualenv env --python=python2.7;
+virtualenv env --python=python3;
+
 source env/bin/activate && pip install --editable ../ray-core;
 source env/bin/activate && pip install --editable ../ray-appengine;
+source env/bin/activate && pip install --editable ../ray-peewee;
+
 # to fix the appengine bug
 source env/bin/activate && pip install PyYAML
 source env/bin/activate && pip install --editable ../ray-sqlalchemy;
-source env/bin/activate && pip install --editable ../ray-peewee;
 
 # to test enviroment
-source env/bin/activate && pip install pytest;
+source env/bin/activate && pip install -r requirements_tests.txt;
