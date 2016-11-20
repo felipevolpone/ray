f06334f884e1636ce25493db7067bd5d482b5564
Leo Cabral <leonardocabral96@gmail.com>
2016-11-20 12:05:35 -0200

    added support to repeated ndb.KeyProperty

--- ray-appengine/ray_appengine/all.py
+++ ray-appengine/ray_appengine/all.py
@@ -13,7 +13,7 @@ class GAEModel(AppEngineModel, Model):
 
     @classmethod
     def to_instance(cls, json_attributes):
-        keys_and_kinds = cls._get_keys_and_kinds()
+        keys_and_properties = cls._get_keys_and_properties()
 
         ancestor_kind, ancestor_key = cls.ancestor()
         if not ancestor_kind:
@@ -27,9 +27,13 @@ class GAEModel(AppEngineModel, Model):
 
         for field_name in json_attributes.keys():
             value = json_attributes[field_name]
-            if field_name in keys_and_kinds:
+            if field_name in keys_and_properties:
                 json_value = json_attributes[field_name]
-                value = ndb.Key(keys_and_kinds[field_name], json_value)
+                kind, repeated = keys_and_properties[field_name]
+                if repeated:
+                    value = [ndb.Key(kind, val) for val in json_value]
+                else:
+                    value = ndb.Key(kind, json_value)
 
             setattr(instance, field_name, value)
         return instance
@@ -76,18 +80,22 @@ class GAEModel(AppEngineModel, Model):
             query = cls.query(ancestor=ndb.Key(ancestor_kind, kwargs[ancestor_key]))
             del kwargs[ancestor_key]
 
+        keys_and_properties = cls._get_keys_and_properties()
         fields_to_filter = set(kwargs.keys())
-        keys = set(cls._get_keys_and_kinds().keys())
+        keys = set(keys_and_properties.keys())
         fields_arent_keys = fields_to_filter - keys
 
         for field in fields_arent_keys:
             query = query.filter(getattr(cls, field) == kwargs[field])
 
         if bool(fields_to_filter & keys):  # check if there are keys in the fields to filter
-            keys_and_kinds = cls._get_keys_and_kinds()
-            for key, kind in keys_and_kinds.items():
+            for key, prop in keys_and_properties.items():
                 key_id = kwargs[key]
-                query = query.filter(getattr(cls, key) == ndb.Key(kind, key_id))
+                kind, repeated = prop
+                if type(key_id) is list and repeated:
+                    query = query.filter(getattr(cls, key).IN(key_id))
+                else:
+                    query = query.filter(getattr(cls, key) == ndb.Key(kind, key_id))
 
         return query.fetch()
 
@@ -111,11 +119,11 @@ class GAEModel(AppEngineModel, Model):
         return entity
 
     @classmethod
-    def _get_keys_and_kinds(cls):
+    def _get_keys_and_properties(cls):
         keys = {}
         for name, property_type in cls._properties.items():
             if isinstance(property_type, ndb.KeyProperty):
-                keys[name] = property_type._kind
+                keys[name] = property_type._kind, property_type._repeated
 
         return keys
 
